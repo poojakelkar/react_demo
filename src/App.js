9a5cffa4150aa7dcab1b3a7af93019ba442f4976
poojakelkar <poojakelkar53@gmail.com>
2020-09-02 20:37:45 +0530

    added new react file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="person">
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
+        <h1>{props.name}</h1>
+        <p>Your age:{props.age}</p>
     </div>
   );
 }
 
 export default App;
+export const DATA = 'ALI';
