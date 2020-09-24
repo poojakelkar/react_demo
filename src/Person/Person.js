c98bd067ec134b591746e91491b7655c781c8ee8
poojakelkar <poojakelkar53@gmail.com>
2020-09-24 18:10:26 +0530

    changes made in  Person.js file

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = (props)=>{
-    return <p>I'm a {props.name} and i'm {props.age}Years old</p>
+    return (
+        <div>
+            <p>I'm a {props.name} and i'm {props.age}Years old</p>
+            <p>{props.children}</p>
+        </div>
+    )
 }
 
 export default person;
