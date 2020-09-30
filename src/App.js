1fb94955c8c35620381814df7175135e16492e27
poojakelkar <poojakelkar53@gmail.com>
2020-09-30 18:06:04 +0530

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-import React,{Component} from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { useState } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-class App extends Component{
-  state = {
-    persons:[
-      {name:'many',age:28},
-      {name:'manty',age:29},
-      {name:'max',age:30}
-    ],
-    otherState:'some other value'
-  }
+const App = () => {
+    const [person, setPerson] = useState({ name: "pooja", age: 22 });
 
-  switchNameHandler = () =>{
-   // console.log('was clicked');
-   //this.state.persons[0].name='max';
-   this.setState({
-    persons: [
-      {name:'mate',age:28},
-      {name:'manu',age:29},
-      {name:'max',age:27}
-    ]
-  })
-
-  }
-
-  render(){
-    return(
-      <div className="App">
-        <h1>Hi, I'm React App</h1>
-        <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[0].age}>My hobbies :dancing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[0].age}/>
-      </div>
+    return (
+        <div className='App'>
+            <h1>Hi, I'm React App</h1>
+            <p>This is really working</p>
+            {/* <button onClick={switchNameHandler()}>Switch Name</button> */}
+            <Person name={person.name} age={person.age} color='fair' />
+        </div>
     );
-    //return React.createElement('div',null,React.createElement('h1',null,'Does It works??'));
-  }
-}
+};
 
 export default App;
-
-
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const person = (props)=>{
+const Person = (props)=>{
+    
     return (
         <div>
-            <p>I'm a {props.name} and i'm {props.age}Years old</p>
+            <p>I'm a {props.name} and i'm {props.age}Years old and i'm {props.color}</p>
             <p>{props.children}</p>
         </div>
     )
 }
 
-export default person;
+export default Person;
