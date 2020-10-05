c293d96c28f6061e0199c9c9606b6ca732adbffe
poojakelkar <poojakelkar53@gmail.com>
2020-10-05 18:28:58 +0530

    changes to app.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const Person = (props)=>{
-    
+const Person = (props) => {
     return (
         <div>
-            <p>I'm a {props.name} and i'm {props.age}Years old and i'm {props.color}</p>
-            <p>{props.children}</p>
+            <p>
+                I'm a {props.name} and i'm {props.age}Years old and i'm{" "}
+                {props.color}
+            </p>
         </div>
-    )
-}
+    );
+};
 
 export default Person;
